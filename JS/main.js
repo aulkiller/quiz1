7b9e5f3f47c8b3d2f6f5315522af2f9db344983b
aulkiller <58225261+aulkiller@users.noreply.github.com>
2020-10-15 16:03:31 +0700

    WAHAHAHA

---
+++ JS/main.js
@@ -0,0 +1,25 @@
+const navSlide = () => {
+    const menubtn = document.querySelector('.menu-btn');
+    const nav = document.querySelector('.nav-items');
+    const navbar = document.querySelector('header');
+    const navitems = document.querySelectorAll('.nav-items li')
+
+    //Navtoggle
+    menubtn.addEventListener('click', () =>{
+        nav.classList.toggle('nav-active');
+        navbar.classList.toggle('nav-test');
+
+        //animasi persatuan
+        navitems.forEach((items,index) => {
+            if(items.style.animation){
+                items.style.animation = '';
+            } else {
+                items.style.animation = `navitemsFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+    });
+
+}
+
+navSlide();
+
